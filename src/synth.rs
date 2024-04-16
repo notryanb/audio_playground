cabc45e29e572b22e668c009e1d185d39c62a935
Ryan Blecher <notryanb@gmail.com>
2024-04-15 21:20:02 -0400

    Using basic phasor implementation and keypresses to change waveform

--- src/synth.rs
+++ src/synth.rs
@@ -7,19 +7,30 @@ use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
 use std::mem::MaybeUninit;
 use std::sync::Arc;
+use std::f32::consts::PI;
 
 use ringbuf::{Consumer, HeapRb, SharedRb};
+
+#[derive(Copy, Clone)]
+enum Waveform {
+    Sine,
+    Square,
+    Triangle,
+    Sawtooth,
+}
 
 struct AudioContext {
     volume: f32,
     freq: f32,
     is_playing: bool,
+    waveform: Waveform,
 }
 
 struct Oscillator {
     ctx: Consumer<AudioContext, Arc<SharedRb<AudioContext, Vec<MaybeUninit<AudioContext>>>>>,
     sample_rate: f32,
     i: f32,
+    phasor: f32,
 }
 
 impl AudioCallback for Oscillator {
@@ -29,9 +40,29 @@ impl AudioCallback for Oscillator {
     fn callback(&mut self, out: &mut [f32]) {
         if let Some(ctx) = self.ctx.pop() {
             for x in out.iter_mut() {
-                *x = (2.0 * std::f32::consts::PI * ctx.freq * (self.i / self.sample_rate)).sin()
-                    * ctx.volume;
+                /*
+                // This formula uses individual sample, but a better one is to use phasor.
+                *x = (2.0 * PI * ctx.freq * (self.i / self.sample_rate)).sin() * ctx.volume;
                 self.i += 1.0;
+                */
+                
+                self.phasor += ctx.freq / self.sample_rate;
+
+                // Clamp the phasor [0, 1);
+                while self.phasor >= 1.0 {
+                    self.phasor -= 1.0;
+                }
+                while self.phasor < 0.0 {
+                    self.phasor += 1.0;
+                }
+
+                *x = match ctx.waveform {
+                    Waveform::Sine => (2.0 * PI * self.phasor).sin() * ctx.volume,
+                    Waveform::Square => if self.phasor < 0.5 { -1.0 * ctx.volume } else { 1.0 * ctx.volume },
+                    Waveform::Triangle => if self.phasor < 0.5 { 4.0 * self.phasor * ctx.volume} else { ((4.0 * (1.0 - self.phasor)) - 1.0) * ctx.volume },
+                    Waveform::Sawtooth => (2.0 * self.phasor - 1.0) * ctx.volume,
+                };
+
             }
         } else {
             for x in out.iter_mut() {
@@ -65,6 +96,7 @@ pub fn run() -> Result<(), String> {
             ctx: cons,
             i: 0.0,
             sample_rate: 44_100.0,
+            phasor: 0.0,
         }
     })?;
 
@@ -90,10 +122,15 @@ pub fn run() -> Result<(), String> {
     canvas.present();
 
     let mut event_pump = sdl_context.event_pump()?;
+    let mut waveform = Waveform::Sine;
 
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
+                Event::KeyDown { keycode: Some(Keycode::A), .. } => waveform = Waveform::Sine,
+                Event::KeyDown { keycode: Some(Keycode::S), .. } => waveform = Waveform::Square,
+                Event::KeyDown { keycode: Some(Keycode::D), .. } => waveform = Waveform::Triangle,
+                Event::KeyDown { keycode: Some(Keycode::F), .. } => waveform = Waveform::Sawtooth,
                 Event::Quit { .. }
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
@@ -113,6 +150,7 @@ pub fn run() -> Result<(), String> {
                 freq,
                 volume,
                 is_playing: true,
+                waveform,
             };
             _ = prod.push(new_ctx);
 
@@ -123,6 +161,7 @@ pub fn run() -> Result<(), String> {
                 freq: 440.0,
                 volume: 0.0,
                 is_playing: false,
+                waveform: Waveform::Sine,
             };
             _ = prod.push(new_ctx);
         }
